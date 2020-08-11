dd3ae25a212c442a5ab24509598a6c0826242204
Peter Godart <ptgodart@gmail.com>
2020-08-10 23:49:22 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="ptgodart",
+    version="0.0.1",
+    author="Peter Godart",
+    author_email="ptgodart@mit.edu",
+    description="Python interface to NASA thermodynamics polynomial fits",
+    url="https://github.com/ptgodart/nasaPoly.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
